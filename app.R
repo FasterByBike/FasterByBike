0594159ce267287d80c6bc70ddb980167fe7f13f
Colin Caine <cmc@cs.man.ac.uk>
2018-02-04 00:28:02 +0000

    app: Add lines data

--- app.R
+++ app.R
@@ -7,6 +7,9 @@ library(sf)
 cycle_infra = read_sf("data/tfgm-cycle-infra.geojson")
 routes_car = read_sf("data/routes_car.geojson")
 lines = read_sf("data/lines.geojson")
+lines_car = subset(lines, fastest == "car")
+lines_bicycle = subset(lines, fastest == "bike")
+lines_transit = subset(lines, fastest == "transit")
 cent = st_centroid(routes_car)
 routes_car$Minutes = routes_car$car_time - routes_car$bicycle_time + 8
 schools = load("data/Schools.Rdata")
@@ -24,6 +27,17 @@ ui <- bootstrapPage(
                 # checkboxInput("legend", "Show legend", TRUE)
   )
 )
+
+top500line <- function(proxy, data, name, colour) {
+  # Complains about invalid map parameter
+  # weightedLine <- function(data, proxy, name, color) {
+  #   addPolylines(map = proxy, data = data$geometry, group = paste("Top 500 by", name), color = colour, weight = data$all / 20000, opacity = 1)
+  # }
+
+  # apply(data, 1, weightedLine, proxy, name, color)
+
+  addPolylines(proxy, data = data, group = paste("Top 500 by", name), color = colour, weight = 2, opacity = 1)
+}
 
 server <- function(input, output, session) {
   
@@ -43,14 +57,17 @@ server <- function(input, output, session) {
     proxy = leafletProxy("map") %>% 
       clearShapes() %>% 
       clearMarkers() %>% 
-      hideGroup("Cycle infrastructure")
+      hideGroup(c("Cycle infrastructure", "Top 500 by bike", "Top 500 by public transit", "Top 500 by car"))
       
     if (input$purpose == "Commuting") {
       proxy %>%
         addPolylines(data = routes_car[routes_car$Minutes > input$range[1] & routes_car$Minutes < input$range[2], ], group = "Car routes", color = ) %>%
         addPolylines(data = cycle_infra, group = "Cycle infrastructure", color = "grey") %>%
+        top500line(lines_bicycle, "bike", "green") %>%
+        top500line(lines_transit, "public transit", "yellow") %>%
+        top500line(lines_car, "car", "red") %>%
         addLayersControl(
-          overlayGroups = c("Cycle infrastructure", "Car routes"),
+          overlayGroups = c("Cycle infrastructure", "Car routes", "Top 500 by bike", "Top 500 by public transit", "Top 500 by car"),
           options = layersControlOptions(collapsed = FALSE),
           position = "topleft"
         )
